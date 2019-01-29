9aa6ff4a699d51b76665a26ff9b704e1a9b55be2
josemak25 <amakirij@gmail.com>
2019-01-29 14:05:11 +0100

    [Feature #163508958] Added user reset password

--- modal.js
+++ modal.js
@@ -6,19 +6,12 @@ window.onclick = event => {
 // Get the modal
 const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 const btn = document.querySelectorAll(".menu");
 
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 btn.forEach(element => {
   element.onclick = () => (modal.style.display = "block");
 });
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = () => (modal.style.display = "none");
-
-// When the user clicks anywhere outside of the modal, close it
-console.log("i am working");
